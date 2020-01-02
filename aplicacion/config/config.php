7286b04ff2a976aa8f77af0107ba9431c96b777f
Alberto <elsrarroyo@gmail.com>
2020-01-02 14:45:39 -0600

    prueba 1

--- aplicacion/config/config.php
+++ aplicacion/config/config.php
@@ -23,8 +23,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost:8080/dsevillajr/';
-
+// $config['base_url'] = 'http://localhost:8080/dsevillajr/';
+$config['base_url'] = 'http://localhost/dsevilla/';
+abc
 /*
 |--------------------------------------------------------------------------
 | Index File
