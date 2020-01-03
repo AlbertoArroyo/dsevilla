ace8538a7b4ff836442a60d41faafbefaa79837b
Alberto <elsrarroyo@gmail.com>
2020-01-03 14:43:04 -0600

    cambio 3

--- aplicacion/config/config.php
+++ aplicacion/config/config.php
@@ -25,7 +25,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 */
 // $config['base_url'] = 'http://localhost:8080/dsevillajr/';
 $config['base_url'] = 'http://localhost/dsevilla/';
-abc
 /*
 |--------------------------------------------------------------------------
 | Index File
